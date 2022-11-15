fff2f65e131c52ac8f96de331c267ad6596bc625
AmangeldiFirst <118159496+AmangeldiFirst@users.noreply.github.com>
2022-11-14 12:29:46 -1200

    'yuklash'

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+<header className='shadow'>
+  <div className="container-fluid py-4 ">
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW0B77RpWHqiRTDrTX3CR3L9gIIgr_jGySblXHna2UNpV6qBW2QeCTt3cjP5L01_e-tw&usqp=CAUhttps://olcha.uz/uploads/images/manufacturer/KK/KK/Kh/1663330153.png" alt="" />
+  </div>
+</header>
+<section id='products'>
+<div className="container-fluid">
+  <div className="row my-4 mx-2">
+    <div className="col-sm-6 col-md-4 col-lg-3 ">
+    <div className="card">
+      <div className="card-body">
+      <img className='img-fluid' src="https://olcha.uz/image/220x220/products/QS4aQGLHtrEp6g2wvLm34DDJAFzTVKW5oGzstyAvb6C8ijhg06Ei0H7Ctqwv.png" alt="" />
+    <a href="#" className="group">  Смартфон Xiaomi Redmi Note 11 6 GB 128GB Сумеречный синий</a>
+    <p>2 462 211 сум</p>
+    <p className='badge bg-warning text-wrap'>266 000 сум x 12 мес</p>
+  <div className='text-center py-2'>  <a href="#" className="border btn btn-light ">Купит</a></div>
+   
+      </div>
+    </div>
+     </div>
+     <div className="col-sm-6 col-md-4 col-lg-3 ">
+    <div className="card">
+      <div className="card-body">
+      <img className='img-fluid' src="https://olcha.uz/image/220x220/products/QS4aQGLHtrEp6g2wvLm34DDJAFzTVKW5oGzstyAvb6C8ijhg06Ei0H7Ctqwv.png" alt="" />
+    <a href="#" className="group">  Смартфон Xiaomi Redmi Note 11 6 GB 128GB Сумеречный синий</a>
+    <p>2 462 211 сум</p>
+    <p className='badge bg-warning text-wrap'>266 000 сум x 12 мес</p>
+  <div className='text-center py-2'>  <a href="#" className="border btn btn-light ">Купит</a></div>
+   
+      </div>
+    </div>
+     </div>
+     <div className="col-sm-6 col-md-4 col-lg-3 ">
+    <div className="card">
+      <div className="card-body">
+      <img className='img-fluid' src="https://olcha.uz/image/220x220/products/QS4aQGLHtrEp6g2wvLm34DDJAFzTVKW5oGzstyAvb6C8ijhg06Ei0H7Ctqwv.png" alt="" />
+    <a href="#" className="group">  Смартфон Xiaomi Redmi Note 11 6 GB 128GB Сумеречный синий</a>
+    <p>2 462 211 сум</p>
+    <p className='badge bg-warning text-wrap'>266 000 сум x 12 мес</p>
+  <div className='text-center py-2'>  <a href="#" className="border btn btn-light ">Купит</a></div>
+   
+      </div>
+    </div>
+     </div>
+     <div className="col-sm-6 col-md-4 col-lg-3 ">
+    <div className="card">
+      <div className="card-body">
+      <img className='img-fluid' src="https://olcha.uz/image/220x220/products/QS4aQGLHtrEp6g2wvLm34DDJAFzTVKW5oGzstyAvb6C8ijhg06Ei0H7Ctqwv.png" alt="" />
+    <a href="#" className="group">  Смартфон Xiaomi Redmi Note 11 6 GB 128GB Сумеречный синий</a>
+    <p>2 462 211 сум</p>
+    <p className='badge bg-warning text-wrap'>266 000 сум x 12 мес</p>
+  <div className='text-center py-2'>  <a href="#" className="border btn btn-light ">Купит</a></div>
+   
+      </div>
+    </div>
+     </div>
+     <div className="col-sm-6 col-md-4 col-lg-3 ">
+    <div className="card">
+      <div className="card-body">
+      <img className='img-fluid' src="https://olcha.uz/image/220x220/products/QS4aQGLHtrEp6g2wvLm34DDJAFzTVKW5oGzstyAvb6C8ijhg06Ei0H7Ctqwv.png" alt="" />
+    <a href="#" className="group">  Смартфон Xiaomi Redmi Note 11 6 GB 128GB Сумеречный синий</a>
+    <p>2 462 211 сум</p>
+    <p className='badge bg-warning text-wrap'>266 000 сум x 12 мес</p>
+  <div className='text-center py-2'>  <a href="#" className="border btn btn-light ">Купит</a></div>
+   
+      </div>
+    </div>
+     </div>
+  </div>
+</div>
+</section>
+
+   </div>
+  );
+}
+
+export default App;
